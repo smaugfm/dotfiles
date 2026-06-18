90eb0233dc3239da8332c8070822bc3b23e28cab
Dmytro Marchuk <dmytromar@wix.com>
2026-06-18 06:09:13 +0000

    Add inotify limit raise

--- startup.sh
+++ startup.sh
@@ -8,3 +8,8 @@ fi
 
 $sudo apt-get update
 $sudo apt-get install -y mc silversearcher-ag screen
+
+# Raise inotify watch limit so IntelliJ IDEA can detect external file changes.
+# https://youtrack.jetbrains.com/articles/SUPPORT-A-1715/Inotify-Watches-Limit-Linux
+echo 'fs.inotify.max_user_watches = 1048576' | $sudo tee /etc/sysctl.d/60-inotify-watches.conf
+$sudo sysctl -p /etc/sysctl.d/60-inotify-watches.conf
